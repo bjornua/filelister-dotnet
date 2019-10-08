88183127b34a2e36a57d3c0b8278776a43828dc6
Bjorn Arnholtz <bjorn.arnholtz@gmail.com>
2019-10-08 15:18:20 +0200

    initial commit

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿// Todo:
+// Print out a list of current dir
+// Recursively print out directory entries
+// Take command line argument
+
+
+
+namespace filelister_dotnet
+{
+
+
+    class Program
+    {
+        static public System.Collections.Generic.IEnumerator<string> RecursiveFileEnumerator(string startDirectory)
+        {
+            var remaining_dirs = new System.Collections.Generic.Stack<string>();
+            remaining_dirs.Push(startDirectory);
+
+            while (true)
+            {
+                var directory = remaining_dirs.Pop();
+            }
+
+        }
+
+        static void Main(string[] args)
+        {
+            // System.Console.WriteLine(item);
+
+        }
+    }
+}
+
+
